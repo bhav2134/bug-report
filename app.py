6498a702aefaa6759b6286bbfd1a2dec652e46fd
Bhavdeep Arora <bhavdeepsa@gmail.com>
2024-03-31 13:37:46 -0400

    update database linkage

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
@@ -28,5 +25,7 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
